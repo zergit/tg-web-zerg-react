c8a5f6364f6cd0f928f99199b1a05654634ff016
Sergey Igrunin <igrunen@bk.ru>
2022-11-08 19:08:14 +0300

    tg4 TS1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import Button from '../Button/Buttton';
 import React from 'react';
-import { useTelegram } from '../hooks/useTelegram';
+import { useTelegram } from '../../hooks/useTelegram';
 import './Header.css';
 
 const Header = () => {
