02e84984011ba976bf323d02fb9572832e06fb4a
Sergey Igrunin <igrunen@bk.ru>
2022-11-11 14:10:55 +0300

    nfgng

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { useTelegram } from '../../hooks/useTelegram';
 import './Header.css';
 
 const Header = () => {
-	const { user, onClose } = useTelegram;
+	const { user, onClose } = useTelegram();
 
 
 	return (
